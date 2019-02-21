92a43b985718924821769517c5b474a4d87c33b2
Yurii Timofeyev <georgius@ural.ru>
2019-02-21 23:28:28 +0500

    Added Bulma

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      ++++++++
+      <section class="hero">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">
+              Todo App
+            </h1>
+          </div>
+        </div>
+      </section>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bulma/css/bulma.css'
+import './assets/bulmaswatch.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
