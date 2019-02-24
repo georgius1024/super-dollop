6ed98507abf288871a4687575f8a998527dfc930
Yurii Timofeev <40800136+georgius1024@users.noreply.github.com>
2019-02-24 13:14:42 +0500

    Uploaded to github-pages

---
+++ precache-manifest.5d91db6be1c78412fcdb054b65407013.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "07d29d599c50ca502866d325637d143e",
+    "url": "/super-dollop/static/media/sort-descending.07d29d59.svg"
+  },
+  {
+    "revision": "0833055fd47ba01d55fea5d3ded4ca21",
+    "url": "/super-dollop/static/media/sort-ascending.0833055f.svg"
+  },
+  {
+    "revision": "720774582ab8d3cb474a",
+    "url": "/super-dollop/static/js/runtime~main.72077458.js"
+  },
+  {
+    "revision": "7bab6f941d968fd91b0d",
+    "url": "/super-dollop/static/js/main.7bab6f94.chunk.js"
+  },
+  {
+    "revision": "c98cf7f5e741dbe31529",
+    "url": "/super-dollop/static/js/2.c98cf7f5.chunk.js"
+  },
+  {
+    "revision": "7bab6f941d968fd91b0d",
+    "url": "/super-dollop/static/css/main.3b285e42.chunk.css"
+  },
+  {
+    "revision": "c98cf7f5e741dbe31529",
+    "url": "/super-dollop/static/css/2.fa712a26.chunk.css"
+  },
+  {
+    "revision": "cf5f89c28ab9fd98f7c3950a8c4c0b97",
+    "url": "/super-dollop/index.html"
+  }
+];
